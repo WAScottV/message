867d66032d0e7589f3c89a83144e79967258d68e
pdhoward <patrick.howard@hotmail.com>
2018-05-23 13:26:47 -0400

    code simplification http

--- api/http.js
+++ api/http.js
@@ -4,33 +4,11 @@
 ////////////////////////////////////////////////////////////////////////
 
 const request =       require('request')
-const config =        require('../config').init()
-
-const apiProfile = process.env.CHAOTICSMS_URL || config.chaoticsms.url
-
-let localStorage = {
-  token: ""
-}
-// Generate a unique token for storing your data on backend server.
-let token = localStorage.token
-
-if (!token) {
-  token = localStorage.token = Math.random().toString(36).substr(-8)
- }
-
-let headers = {
-  'Accept': 'application/json',
-  'Authorization': token
- }
-
-//////////////////////////////////////////////////////////////////////////
-////////////////////    execute an http call         ////////////////////
-////////////////////////////////////////////////////////////////////////
 
 // cloud functions residing on microservices platforms like ow, aws, google
-// note in post request, 'json' is a key word -- as well as 'form'
-// ie {json: { key: value }}
-  exports.getAgentResponse = (apiparm) => {  
+// note in post request, 'json' is a key word -- ie {json: { key: value }}
+
+  exports.getAgentResponse = (apiparm) => {
        return new Promise((resolve, reject) => {
          request.post(
               apiparm.url,
