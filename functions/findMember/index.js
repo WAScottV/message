1d4433dfb2e92382cb4e765239d46a1aa1c6aea8
pdhoward <patrick.howard@hotmail.com>
2018-06-07 12:40:15 -0400

    find member function fix

--- functions/findMember/index.js
+++ functions/findMember/index.js
@@ -8,22 +8,29 @@
 const dbc =       require('../../api/db')
 const {db} =      require('../../db')
 
-exports.findMember = (obj, conn) => {
-  console.log("findmember in message")
-  console.log(obj)
-  console.log(conn)
-/*
-  db()
-  .then((d) => {
-    let obj = d.getWorkObj()
-    let conn = d.getConnection()
-    return new Promise((resolve, reject) => {
-      resolve(dbc.findMember(obj, conn))
-      })
+exports.findMember = async () => {
+  let obj = await getWorkObj
+  let conn = await getConnection
+  let member = await getMember(obj, conn)
+  return member
+}
+
+const getWorkObj = () => {
+  return new Promise ((resolve, reject) => {
+    db()
+    .then((d) => {
+      resolve(d.getWorkObj()) })
   })
-*/
+}
+const getConnection = () => {
+  return new Promise ((resolve, reject) => {
+    db()
+    .then((d) => {
+      resolve(d.getConnection()) })
+  })
+}
+const getMember = (obj, conn) => {
   return new Promise((resolve, reject) => {
     resolve(dbc.findMember(obj, conn))
   })
-
 }
